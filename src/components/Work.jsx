31ee9f7e612f8aefa2ba58c7150e8c9552306d94
stens0n <astenson010@gmail.com>
2023-01-18 08:32:22 -0500

    creation of work component

---
+++ src/components/Work.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div name='work' class="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+        <div>
+            <div>
+                <p>Work</p>
+                <p>// Check out some of my recent work</p>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Work
